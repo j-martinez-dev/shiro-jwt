667f73530c1d5f3cd02c64a3e5bfcca980e9d99b
Jaime Martinez <panchitoboy@gmail.com>
2015-09-06 12:20:58 +0200

    - Add DeltaSpike Configuration

--- src/main/java/com/github/panchitoboy/shiro/rest/SecurityExceptionMapper.java
+++ src/main/java/com/github/panchitoboy/shiro/rest/SecurityExceptionMapper.java
@@ -1,5 +1,9 @@
 package com.github.panchitoboy.shiro.rest;
 
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +12,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import org.apache.deltaspike.core.api.config.ConfigProperty;
 import org.apache.shiro.ShiroException;
 
 @Provider
@@ -16,11 +21,26 @@ public class SecurityExceptionMapper implements ExceptionMapper<ShiroException> 
     @Context
     HttpServletRequest req;
 
+    @Inject
+    @ConfigProperty(name = "security.exception.messages.file", defaultValue = "messages")
+    String resourceBundleFile;
+
     @Override
     public Response toResponse(ShiroException exception) {
         JsonArrayBuilder array = Json.createArrayBuilder();
-        array.add(exception.getMessage());
+        array.add(getMessage(exception.getMessage(), req));
         return Response.status(Response.Status.BAD_REQUEST).entity(array.build()).type(MediaType.APPLICATION_JSON).build();
+    }
+
+    private String getMessage(String key, HttpServletRequest req) {
+        Locale currentLocale = req.getLocale().stripExtensions();
+        try {
+            ResourceBundle resourceBundle = ResourceBundle.getBundle(resourceBundleFile, currentLocale);
+            String message = resourceBundle.getString(key);
+            return message;
+        } catch (MissingResourceException ex) {
+            return key;
+        }
     }
 
 }
